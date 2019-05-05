d5cb775aa6758edef4908d1077fb523b13b07918
Eduardo Tavares <edutav@gmail.com>
2019-05-05 10:03:54 -0400

    simplificação das chamadas dos controllers nas rotas

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const routes = express.Router();
 
-const UserController = require('./app/controller/UserController');
+const authMiddleware = require('./app/middlewares/auth');
 
-routes.post('/users', UserController.store);
+const controllers = require('./app/controller');
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }));
+
+routes.post('/users', controllers.UserController.store);
+routes.post('/session', controllers.SessionController.store);
 
 module.exports = routes;
