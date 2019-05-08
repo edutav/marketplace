a15283d9d202e4157c25468afcd52e3b6ec10b04
Eduardo Tavares <edutav@gmail.com>
2019-05-07 22:59:02 -0400

    implemantação da fila para envio de email

---
+++ src/app/jobs/PurchaseMail.js
@@ -0,0 +1,23 @@
+const Mail = require('../services/Mail');
+
+class PurchaseMail {
+	get key () {
+		return 'PurchaseMail';
+	}
+
+	async handle (jobs, done) {
+		const { ad, user, content } = jobs.data;
+
+		await Mail.sendMail({
+			from: '"Eduardo Tavares" <edutav@gmail.com>',
+			to: ad.author.email,
+			subject: `Solicitação de compra: ${ad.title}`,
+			template: 'purchase',
+			context: { user, content, ad }
+		});
+
+		return done();
+	}
+}
+
+module.exports = new PurchaseMail();
