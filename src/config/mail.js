d42dfc2da027da4614a0288a328b750401a287fc
Eduardo Tavares <edutav@gmail.com>
2019-05-07 22:57:23 -0400

    alterando o valor para variavel de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-	host: 'smtp.mailtrap.io',
-	port: 2525,
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
 	secure: false,
 	auth: {
-		user: '80005789b3abc2',
-		pass: '717cd4c05b8890'
+		user: process.env.MAIL_USER,
+		pass: process.env.MAIL_PASS
 	}
 };
